44b2c883daaf4d1b53ba7002fbe355265c8033b6
kayalvizhi <madhukayal88@gmail.com>
2026-08-13 17:54:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,31 +2,29 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const handleClick = () => {
-    alert('Button clicked!');
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log('Form submitted');
   };
 
   const handleChange = (e) => {
     console.log(e.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert('Form submitted!');
-  };
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>JSX Keyword Replacement Challenge</h1>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="username">Username</label>
+        
+        <form onSubmit={handleClick}>
+          <label htmlFor="username">Username:</label>
           <input
             type="text"
             id="username"
             name="username"
             onChange={handleChange}
             autoComplete="off"
+            tabIndex="1"
           />
 
           <button type="submit" className="btn" onClick={handleClick}>
